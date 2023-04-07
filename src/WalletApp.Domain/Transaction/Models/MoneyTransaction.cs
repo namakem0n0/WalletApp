9fa06143029310b16260c91f58fdf82804e799a6
Rostyslav <rrrostyk@gmail.com>
2023-04-08 01:11:55 +0300

    refactor-moneyTransaction-update

--- src/WalletApp.Domain/Transaction/Models/MoneyTransaction.cs
+++ src/WalletApp.Domain/Transaction/Models/MoneyTransaction.cs
@@ -5,13 +5,13 @@ namespace WalletApp.Domain.Transaction.Models
     public class MoneyTransaction
     {
         public int Id { get; private set; }
-        public string Type { get; private set; }
-        public decimal Amount { get; private set; }
         public string Name { get; private set; }
         public string Description { get; private set; }
+        public decimal Amount { get; private set; }
+        public int UserId { get; private set; }
+        public TransactionType Type { get; private set; }
         public DateTime Date { get; private set; }
         public bool IsPending { get; private set; }
-        public int UserId { get; private set; }
 
         public User AuthorizedUser { get; private set; }
     }
