70c23361377ab2aeceb1e90f897de033f47e8797
Rostyslav <rrrostyk@gmail.com>
2023-04-10 00:01:55 +0300

    removed-root-folder-for-card-model

--- src/WalletApp.Infrastructure/Common/UnitOfWork.cs
+++ src/WalletApp.Infrastructure/Common/UnitOfWork.cs
@@ -1,8 +1,6 @@
-﻿using WalletApp.Domain.Cards.Common;
-using WalletApp.Domain.Common;
+﻿using WalletApp.Domain.Common;
 using WalletApp.Domain.Transactions.Common;
 using WalletApp.Domain.Users.Common;
-using WalletApp.Infrastructure.Cards.Common;
 using WalletApp.Infrastructure.MoneyTransactions.Common;
 using WalletApp.Infrastructure.Users.Common;
 using WalletApp.Persistence.Context;
